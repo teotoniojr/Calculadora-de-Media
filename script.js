d6dcec89bf95c00b42db492e53da338eb3f5d150
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2022-09-06 15:35:15 -0300

    Aparecendo na tela

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var nome = "Teotonio"
-var notaPrimeiroTrimestre = 9
-var notaSegundoTrimestre = 5
-var notaTerceiroTrimestre = 6
-var notaFinal = (notaPrimeiroTrimestre + notaSegundoTrimestre + notaTerceiroTrimestre) / 3
+function Calcular () {
+    var elementoPrimeiraNota = document.getElementById("primeiraNota") 
+    var primeiraNota = elementoPrimeiraNota.value
+    var notaUmNumerico = parseFloat(primeiraNota)
+    
+    var elementoSegundaNota = document.getElementById("segundaNota") 
+    var segundaNota = elementoSegundaNota.value
+    var notaDoisNumerico = parseFloat(segundaNota)
 
-var notaFixada = notaFinal.toFixed(1)
- 
-console.log ("Bem vindo " + nome)
-console.log (notaFixada)
+    var elementoTerceiraNota = document.getElementById("terceiraNota") 
+    var terceiraNota = elementoTerceiraNota.value
+    var notaTresNumerico = parseFloat(terceiraNota)
+    
 
-notaFixada.innerHTML = "Sua média é "
+    var media = (notaUmNumerico + notaDoisNumerico + notaTresNumerico)/3
+    console.log(media)
+
+    var elementoMediaFinal = document.getElementById("mediaFinal")
+    var mediaFinal = "A sua média final é de : " + media
+    elementoMediaFinal.innerHTML = mediaFinal
+}
