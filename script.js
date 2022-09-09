3a1c85bee8261ec772c73d636a99cbba7547294d
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2022-09-08 21:05:35 -0300

    Limpando código e incrementando resultado

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function Calcular () {
-    var elementoPrimeiraNota = document.getElementById("primeiraNota") 
-    var primeiraNota = elementoPrimeiraNota.value
-    var notaUmNumerico = parseFloat(primeiraNota)
+    var primeiraNota = parseFloat(document.getElementById("primeiraNota").value) 
+    var segundaNota = parseFloat(document.getElementById("segundaNota").value) 
+    var terceiraNota = parseFloat(document.getElementById("terceiraNota").value) 
+   
+    var media = (primeiraNota + segundaNota + terceiraNota)/3
     
-    var elementoSegundaNota = document.getElementById("segundaNota") 
-    var segundaNota = elementoSegundaNota.value
-    var notaDoisNumerico = parseFloat(segundaNota)
-
-    var elementoTerceiraNota = document.getElementById("terceiraNota") 
-    var terceiraNota = elementoTerceiraNota.value
-    var notaTresNumerico = parseFloat(terceiraNota)
-    
-
-    var media = (notaUmNumerico + notaDoisNumerico + notaTresNumerico)/3
-    console.log(media)
 
     var elementoMediaFinal = document.getElementById("mediaFinal")
     var mediaFinal = "A sua média final é de : " + media
     elementoMediaFinal.innerHTML = mediaFinal
-}
+
+    var elementoResultado = document.getElementById("resultado")
+
+    if (media >= 6) {
+        elementoResultado.innerHTML = " Parabéns! Você concluiu este ciclo. Está pronto para começar novas experiências"
+    } else if (media < 6 || media >= 3) {
+        elementoResultado.innerHTML = " Opa, quase hein... É necessário recuperar essa nota, para conseguir vencer esse ciclo!"
+      }  else {
+            elementoResultado.innerHTML = " Podemos fazer melhor da próxima vez! Não desista"
+        }
+    }
